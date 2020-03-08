b1622459c42d93d537a5130b712054a90e7e9965
yerowyn <yerowyn@gmail.com>
2020-03-08 20:46:59 +0600

    add user context to root

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from 'routes';
-import TopBar from 'components/TopBar.js'
+import TopBar from 'components/TopBar.js';
+import {CurrentUserProvider} from 'contexts/currentUser';
+import CurrenUserChecker from 'components/currentUserChecker';
 
 const App = (props) => {
 	return (
-			<div>
-				<Router>
-					<TopBar />
-					<Routes />
-				</Router>
-			</div>
+			<CurrentUserProvider>
+				<CurrenUserChecker>
+					<Router>
+						<TopBar />
+						<Routes />
+					</Router>
+				</CurrenUserChecker>
+			</CurrentUserProvider>
 	);
 }
 
